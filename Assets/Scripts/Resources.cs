9f58d9745b59e796984dedb94fcef93ab1880064
Noki <ndinh@stud.hs-bremen.de>
2017-05-21 17:04:17 +0200

    Resources

---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : MonoBehaviour {
+
+	public int Population;
+	public int Money;
+	public int Food;
+	public int Water;
+
+	public float PopulationIncrease; //increase in percentage
+	public int MoneyIncrease;
+	public int FoodIncrease;
+	public int WaterIncrease;
+
+	// Use this for initialization
+	void Start () {
+		//set default resources
+		Population = 1000;
+		Money = 400000;
+		Water = 0;
+		Food = 0;
+
+		PopulationIncrease = 1.1f; //10% increase in 4 years
+
+		MoneyIncrease = 0;
+		FoodIncrease = 0;
+		WaterIncrease = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void Reset()
+	{
+		MoneyIncrease = 0;
+		FoodIncrease = 0;
+		WaterIncrease = 0;
+	}
+}
