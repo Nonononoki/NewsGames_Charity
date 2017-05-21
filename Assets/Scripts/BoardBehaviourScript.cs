3e3ea2b861c40159d3e646fa2d648dc98c8140d6
Noki <ndinh@stud.hs-bremen.de>
2017-05-21 16:25:33 +0200

    - default scene

---
+++ Assets/Scripts/BoardBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardBehaviourScript : MonoBehaviour {
+
+	bool hasObject=false;
+	public Transform cube;
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+	}
+
+	void OnMouseDown(){
+		if (hasObject == false) {
+			Instantiate (cube, transform.position, Quaternion.identity);
+		}
+		hasObject = true;
+	}
+
+
+}
