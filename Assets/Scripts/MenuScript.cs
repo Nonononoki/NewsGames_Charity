3eddd1b06566dd24dc4f0d827f4e8c41a3cc58a0
Noki <ndinh@stud.hs-bremen.de>
2017-05-22 11:21:12 +0200

    Signed-off-by: Noki <ndinh@stud.hs-bremen.de>

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -41,10 +41,24 @@ public class MenuScript : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.I)) 
 		{
 			//Display Info
+			PrintInfo();
 		}
 		if (Input.GetKeyDown (KeyCode.Space)) 
 		{
 			nrb.NextRound ();
 		}
+	}
+
+	void PrintInfo()
+	{
+		Debug.Log (
+			"Pro Runde erhaelt der Spieler 400.000€ um seine Projekte zu realisieren.\n" +
+			"Jedes Dorf verbraucht am Ende des Zuges Nahrung und Wasser proportional zur Bevoelkerung.\n" +
+			"Jede Runde waechst die Bevoelkerung um 10 Prozent.\n" +
+			"Farmen produzieren Nahrung, Brunnen produzieren Wasser \n" + 
+			"Wenn der Spieler den Verbrauch nicht decken kann, muss der Spieler fuer die Deckung zahlen\n" +
+			"Wenn das Geld auf 0 sinkt, verliert der Spieler.\n" +
+			"Die Schule erhoeht jede Runde den Bildungsgrad, ist dieser bei 100% angelangt ist das Spiel gewonnen.\n" +
+			"Gebauede besitzen Bebauungs- und Verwaltungskosten.");
 	}
 }
